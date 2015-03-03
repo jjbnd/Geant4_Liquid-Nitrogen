efa5725402fbb180d845fb2f062ee616ac9c7dc8
Jae-Yong <develjy@gmail.com>
2015-03-03 16:03:37 +0900

    컴파일 warning 해결

--- src/RunAction.cc
+++ src/RunAction.cc
@@ -33,7 +33,7 @@ void RunAction::BeginOfRunAction(const G4Run*)
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-void RunAction::EndOfRunAction(const G4Run* run)
+void RunAction::EndOfRunAction(const G4Run*)
 {
 	THFile* file = THFile::Instance();
 	file->Close();
